56f04b0be0cc0393d78c42a50cdb79599db0740a
Emanuele Cisbani <emanuele.cisbani@gmail.com>
2019-12-22 20:32:49 +0100

    refined contrib/tsacert.py

--- contrib/tsacert.py
+++ contrib/tsacert.py
@@ -3,16 +3,19 @@
 openssl x509 -text -in tsa.crt | grep -A 3 "Authority Information Access"
 '''
 
+import sys
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 
-with open("tsa.crt", "rb") as tsa_der:
-    cert = x509.load_pem_x509_certificate(tsa_der.read(), default_backend())
+with open(sys.argv[1], "rb") as tsa_pem:
+    cert = x509.load_pem_x509_certificate(tsa_pem.read(), default_backend())
 for rdns in cert.subject.rdns:
     #print(dir(rdns._attributes))
     for a in rdns._attributes:
-        if a.oid._name in ("organizationName", "commonName"):
-            print("%s - %s" % (a.oid._name, a.value))
+        if a.oid._name == "organizationName":
+            print("Organization = %s" % a.value)
+        if a.oid._name == "commonName":
+            print("Common Name = %s" % a.value)
 
 #for e in cert.extensions:
 #    print(e.oid, e.value)
@@ -21,15 +24,15 @@ from cryptography.x509.oid import ExtensionOID
 #print(dir(ExtensionOID))
 
 e = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_KEY_IDENTIFIER)
-print("SUBJECT_KEY_IDENTIFIER")
-print(e.value.digest.hex())
+print("Subject Key Identifier = %s" % e.value.digest.hex())
 
 e = cert.extensions.get_extension_for_oid(ExtensionOID.AUTHORITY_INFORMATION_ACCESS)
-print("AUTHORITY_INFORMATION_ACCESS")
+print("Authority Information Access:")
 for item in e.value:
-    print(item.access_method.dotted_string, item.access_method._name, item.access_location.value)
-print("CRL_DISTRIBUTION_POINTS")
+    #print(item.access_method.dotted_string, item.access_method._name, item.access_location.value)
+    print("- %s = %s" % (item.access_method._name, item.access_location.value))
+print("CRL Distribution Points:")
 e = cert.extensions.get_extension_for_oid(ExtensionOID.CRL_DISTRIBUTION_POINTS)
 for item in e.value:
     for url in item.full_name:
-        print(url.value)
+        print("- %s" % url.value)
