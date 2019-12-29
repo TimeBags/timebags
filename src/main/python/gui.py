17d0bdc523b1911d593ab69ccef3c4f8da4b1205
Emanuele Cisbani <emanuele.cisbani@gmail.com>
2019-12-29 19:52:07 +0100

    menu help completed

--- src/main/python/gui.py
+++ src/main/python/gui.py
@@ -19,17 +19,19 @@ import sys
 
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QWidget, QVBoxLayout,\
-QFileDialog, QMessageBox, QDialog, QDialogButtonBox, QGroupBox, QFormLayout
+QFileDialog, QMessageBox, QDialog, QDialogButtonBox, QGroupBox, QFormLayout, QAction, \
+QPlainTextEdit
+from PyQt5.QtGui import QFontDatabase, QTextCursor
 
 import core
+import gplv3
 
 
-
-class MyDialog(QDialog):
+class ResultDialog(QDialog):
     ''' Dialog to display the result '''
 
     def __init__(self, result):
-        super(MyDialog, self).__init__()
+        super(ResultDialog, self).__init__()
         self.create_form_groupbox(result)
 
         button_box = QDialogButtonBox(QDialogButtonBox.Ok)
@@ -59,9 +61,108 @@ class MyDialog(QDialog):
         self.form_groupbox.setLayout(layout)
 
 
+class AboutDialog(QDialog):
+    ''' Dialog to display the about info '''
+
+    def __init__(self, version):
+        super(AboutDialog, self).__init__()
+        self.create_about_groupbox(version)
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok)
+        button_box.accepted.connect(self.accept)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(10, 10, 10, 10)
+        layout.addWidget(self.form_groupbox)
+        layout.addWidget(button_box)
+        self.setLayout(layout)
+        self.setWindowTitle("About")
+
+
+    def create_about_groupbox(self, version):
+        ''' Form to display the about info '''
+
+        url = "<a href=\"https://timebags.org\">https://timebags.org</a>"
+        url_label = QLabel(url)
+        url_label.setOpenExternalLinks(True)
+        self.form_groupbox = QGroupBox("TimeBags software informations")
+        layout = QFormLayout()
+        layout.addRow(QLabel("Website Url:"), url_label)
+        layout.addRow(QLabel("Version:"), QLabel(str(version)))
+        layout.addRow(QLabel("Copyright:"), QLabel("2019-2020 Emanuele Cisbani"))
+        layout.addRow(QLabel("License:"), QLabel("GNU General Public License 3"))
+        self.form_groupbox.setLayout(layout)
+
+
+
+class LicenseDialog(QDialog):
+    ''' Dialog to display the License '''
+
+    def __init__(self):
+        super(LicenseDialog, self).__init__()
+
+        self.resize(650, 450)
+
+        license_text = QPlainTextEdit()
+        license_text.setReadOnly(True)
+        fixed_font = QFontDatabase.systemFont(QFontDatabase.FixedFont)
+        license_text.setFont(fixed_font)
+        license_text.insertPlainText(gplv3.get_txt())
+        license_text.moveCursor(QTextCursor.Start)
+        license_text.ensureCursorVisible()
+
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok)
+        button_box.accepted.connect(self.accept)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(10, 10, 10, 10)
+        layout.addWidget(license_text)
+        layout.addWidget(button_box)
+        self.setLayout(layout)
+        self.setWindowTitle("About")
+
+
+
 
 class MyMainWindow(QMainWindow):
     ''' Main Window '''
+
+    def __init__(self, version):
+
+        super(MyMainWindow, self).__init__()
+
+        self.resize(350, 100)
+        self.setWindowTitle("Put your files in TimeBags!")
+        self.setCentralWidget(self.central_widget())
+        self.create_menubar()
+        self.version = version
+
+    def about_dialog(self):
+        dialog = AboutDialog(self.version)
+        dialog.exec_()
+
+    def license_dialog(self):
+        dialog = LicenseDialog()
+        dialog.exec_()
+
+    def create_menubar(self):
+        ''' Menu Bar '''
+
+        main_menu = self.menuBar()
+        help_menu = main_menu.addMenu('Help')
+
+        about_btn = QAction('About', self)
+        about_btn.triggered.connect(self.about_dialog)
+        help_menu.addAction(about_btn)
+
+        license_btn = QAction('License', self)
+        license_btn.triggered.connect(self.license_dialog)
+        help_menu.addAction(license_btn)
+
+        exit_btn = QAction('Exit', self)
+        exit_btn.triggered.connect(self.close)
+        help_menu.addAction(exit_btn)
 
     def get_save_filename(self):
         ''' Ask for a filename to save new timebag '''
@@ -106,7 +207,7 @@ class MyMainWindow(QMainWindow):
         if files:
             ret = core.main(files, self.get_save_filename)
             if ret is not None:
-                dialog = MyDialog(ret)
+                dialog = ResultDialog(ret)
                 dialog.exec_()
             else:
                 msg = "Some error occurred.\nSee the log file for details."
@@ -140,11 +241,8 @@ class AppContext(ApplicationContext):
     def run(self):
         ''' App execution '''
 
-        window = MyMainWindow()
         version = self.build_settings['version']
-        window.setWindowTitle("TimeBags ver. %s" % version)
-        window.resize(250, 150)
-        window.setCentralWidget(window.central_widget())
+        window = MyMainWindow(version)
         window.show()
         return self.app.exec_()
 
